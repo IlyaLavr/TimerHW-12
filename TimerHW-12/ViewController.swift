a6deadf0b657671b7e089d2737010d37f104f877
Ilya <iilavr@mail.ru>
2022-08-28 16:27:09 +0300

    Initial Commit

---
+++ TimerHW-12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimerHW-12
+//
+//  Created by Илья on 28.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
